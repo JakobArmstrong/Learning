98889984037605c8c5196d7eb7efa01742ed7551
JakobArmstrong <jakobholamarmstrong.com>
2021-11-24 10:16:22 -0500

    Introduced sum_array

---
+++ sum_array/sum_array.cpp
@@ -0,0 +1,23 @@
+// Prompts the user for length of array and inputs each index
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size;
+    int sum;
+
+    cout << "\nWhat is the size of your array? ";
+    cin >> size;
+    int array[size];
+
+    cout << "\nWhat are the values for each index? \n";
+    for (int i=0; i<size; i++){
+        cin >> array[i];
+    }
+
+    for (int i=0; i<size; i++){
+        sum += array[i];
+    }
+    cout << "\nThe sum of values in the array is: " << sum;
+}
